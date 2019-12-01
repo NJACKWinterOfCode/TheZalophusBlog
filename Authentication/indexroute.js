c44ccf8e9d648cc847c5ef98b838ddd5d9a7ea24
RitwickGhosh <56272528+RitwickGhosh@users.noreply.github.com>
2019-12-01 22:27:16 +0530

    Create indexroute.js

---
+++ Authentication/indexroute.js
@@ -0,0 +1,99 @@
+var express     = require("express"),
+
+    User        = require("../models/usermodel"),
+
+    passport    = require("passport"),
+
+    router      = express.Router({mergeParams: true});
+
+
+
+// HOME PAGE
+
+router.get("/", function(req, res){
+
+    res.render("landing");
+
+})
+
+
+
+//REGISTRATION
+
+router.get("/register", function(req, res){
+
+    res.render("register");
+
+});
+
+
+
+router.post("/register", function(req, res){
+
+    var newUser = new User({username: req.body.username});
+
+    User.register(newUser, req.body.password, function(err, user){
+
+        if(err){
+
+            req.flash("error", err.message);
+
+            return res.redirect("register");
+
+        }
+
+        passport.authenticate("local")(req, res, function(){
+
+            req.flash("success","Welcome " + user.username);
+
+            res.redirect("/blogs");
+
+        })
+
+    });
+
+});
+
+
+
+// LOGIN
+
+router.get("/login", function(req, res){
+
+    res.render("login");
+
+});
+
+
+
+router.post("/login", passport.authenticate("local",
+
+    {
+
+        successRedirect: "/blogs",
+
+        failureRedirect: "/login",
+
+        failureFlash: true
+
+    }), function(req, res){
+
+});
+
+
+
+//LOGOUT 
+
+router.get("/logout", function(req, res){
+
+    req.logout();
+
+    req.flash("success","Logged you out!");
+
+    res.redirect("/blogs");
+
+});
+
+
+
+module.exports = router;
